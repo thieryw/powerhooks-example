c8b9e2eb84c5f201344230ceb2df8acf44c68b92
William Thiery <williamalexthiery@hotmail.fr>
2021-02-16 19:49:14 +0100

    update

--- src/components/UseCallbackFactoryExample.tsx
+++ src/components/UseCallbackFactoryExample.tsx
@@ -9,12 +9,45 @@ import {memo} from "react";
 export const UseCallbackFactoryExample = ()=>{
 
 
-
     return(
-        <div>
-     
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }}>
+            <h1>useCallbackFactory</h1>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(3,1fr)",
+                gridGap: "3px",
+                backgroundColor: "black",
+                width: "400px",
+                border: "solid black 3px"
+            }}>
+
+                {
+                    [0,1,2,3,4,5,6,7,8].map(cellNumber => <Cell/>)
+                }
+            </div>
         </div>
     )
 }
+
+
+
+const Cell = memo(()=>{
+
+
+    return(
+        <div style={{
+            backgroundColor: "white",
+            height: "100px"
+        }}>
+            
+        </div>
+    )
+})
+
+
 
 
